e317ddbfa10858ddb7b4da201a8fa511702e26e1
Nozhan Amiri <nozhana@gmail.com>
2024-01-22 21:54:06 +0330

    Alpha 4

--- Sources/GSnackbar/SnackbarData.swift
+++ Sources/GSnackbar/SnackbarData.swift
@@ -10,9 +10,9 @@ import SwiftUI
 public struct SnackbarData: Equatable {
     public var title: String
     public var description: String
-    public var buttons: [SnackbarButton] = []
+    public var buttons: [SnackbarButton]
     
-    public init(title: String, description: String, buttons: [SnackbarButton]) {
+    public init(title: String, description: String, buttons: [SnackbarButton] = []) {
         self.title = title
         self.description = description
         self.buttons = buttons
